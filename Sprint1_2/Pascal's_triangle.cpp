9b32b5898d7a765e3418dc1905f37ef1c2da6316
Olha <olhalesk@gmail.com>
2016-12-25 23:43:19 +0200

    Pascal's triangle

---
+++ Sprint1_2/Pascal's_triangle.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+void pascals_triangle(int height);
+
+int main()
+{
+    int height = 0;
+    cout << "Input the height of a triangle: ";
+    cin >> height;
+    
+    pascals_triangle(height);
+    
+    return 0;
+}
+
+
+void pascals_triangle(int height)
+{
+    int numbers[height];
+    for (int i = 0; i < height; i++)
+    {
+        numbers[i] = 0;
+    }
+    
+    numbers[0] = 1;
+    for (int row = 1; row <= height; row++)
+    {
+        for(int j = 1; j < height-row+1; j++)
+            cout << " ";
+        for (int col = row; col >= 1; col--)
+        {
+            cout << numbers[col-1] << " ";
+            numbers[col] = numbers[col-1] + numbers[col];
+        }
+        cout << endl;
+    }
+}
+/*
+for(int i=0;i<h;i++)
+{
+    for(int j=1;j<h-i;j++)
+        cout<<'-';
+        for(int j=h-i;j<=h+i;j++)
+        cout<<'*';
+        for(int j=h+i+1;j<h*2;j++)
+        cout<<'-';
+        cout<<endl;
+        }
+*/
